c18698064f6b3ee901c28b22c3b4fbdb1b452a6a
Yume SAIKO <hei.hajatiana@gmail.com>
2022-03-01 03:31:20 +0300

    push

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <Character.h>
+#include <vector>
+#include <numeric>
+#include <inttypes.h>
+
+using namespace std;
+
+// a function with no return, takes in argument an instance of the class Character
+void introduce_Char(Character const *character) {
+    character->about();
+}
+
+int main()
+{
+
+    // polymorphism function requires pointer/reference and virtual class
+    Character *Mokujin(0);
+    Mokujin = new Character("Mokujin", "N\a", 100, 35);
+
+    // the class Mishima is another class that inherited the class Character
+    Mishima *Kazuya(0);
+    Kazuya = new Mishima("Kazuya", "dorya",100, 38, "blue");
+
+
+    // function about (character->about) has the same name but different method to introduce a character
+    introduce_Char(Mokujin);
+    introduce_Char(Kazuya);
+
+
+    return 0;
+}
+
+
